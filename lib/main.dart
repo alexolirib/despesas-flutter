11f7e225b1b1c06cae4de1402f1588bf9aea4e3d
Alexandre Oliveira Ribeiro <alexolirib@gmail.com>
2020-05-01 12:41:18 -0300

    criado o objeto transacao do projeto e organizando a interface

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:despesas/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(ExpensesApp());
@@ -5,24 +6,62 @@ main() => runApp(ExpensesApp());
 class ExpensesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MyHomePage()
-      
-    );
+    return MaterialApp(home: MyHomePage());
   }
 }
 
 class MyHomePage extends StatelessWidget {
+  final _transaction = [
+    Transaction(
+      id: 't1',
+      title: 'Novo tênis de corrida',
+      value: 310.76,
+      date: DateTime.now()
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de luz',
+      value: 215.10, 
+      date: DateTime.now()
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Despesas Pessoais'),
       ),
-      body: Center(
-        child: Text('body app'),
-        ),
-      
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            // width: double.infinity, - não precisa por conta do CrossAxisAlignment.stretch
+            child: Card(
+                color: Colors.blue,
+                child: Text('Grafico'),
+                elevation: 5
+            )
+          ),
+          Column(
+            children: _transaction.map((tr) {
+                return Card(
+                  child: Row(children: <Widget>[
+                      Container(
+                        child: Text(tr.value.toString()) ,
+                      ),
+                      Column(children: <Widget>[
+                        Text(tr.title),
+                        Text(tr.date.toString())
+                      ],)
+                    ],
+                  )
+                );
+              }).toList()            
+          )
+        ],
+      ),
     );
   }
 }
